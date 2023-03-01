949fa1b5cbe54d938907d8d391d4017691cf20a1
Pedro <Pedro@DESKTOP-U43HP29>
2023-03-01 14:51:22 +0000

    criar print left an rigth

--- src/pt/pedrogomes/caixa/Main.java
+++ src/pt/pedrogomes/caixa/Main.java
@@ -1,17 +1,29 @@
 package pt.pedrogomes.caixa;
 
+import java.util.Iterator;
+
 public class Main {
 
 	public static void main(String[] args) {
 		printTOpOrBottom(4);
-		System.out.println("*       *");
-		System.out.println("*       *");
-		System.out.println("*       *");
+		printLeftandRigth(4, 2);
 		printTOpOrBottom(4);
 
 	}
 	public static void printTOpOrBottom(int size) {
-		for(int i = 0 ; i <size ; i++)
+		for(int i = 0 ; i <size ; i++) {
 		System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	public static void printLeftandRigth(int width, int height) {
+		for (int i = 0; i < height; i++) {
+			System.out.print("*");
+			for (int j = 0; j < width - 2; j++) {
+				System.out.print(" ");
+			}
+			System.out.println("*");
+		}
 	}
 }
