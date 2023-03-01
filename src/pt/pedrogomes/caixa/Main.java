f2d1def4c853e40dba4287c66d37accd0aac71e5
Pedro <Pedro@DESKTOP-U43HP29>
2023-03-01 14:11:38 +0000

    second commit

--- src/pt/pedrogomes/caixa/Main.java
+++ src/pt/pedrogomes/caixa/Main.java
@@ -3,7 +3,11 @@ package pt.pedrogomes.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("*********");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*       *");
+		System.out.println("*********");
 
 	}
 
