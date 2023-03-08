8f80cdbd561f5ac6b94dc9b2fc668aa461bae45f
Pedro <Pedro@DESKTOP-U43HP29>
2023-03-08 13:51:13 +0000

    add

--- src/pt/pedrogomes/caixa/Caixa.java
+++ src/pt/pedrogomes/caixa/Caixa.java
@@ -4,12 +4,16 @@ public class Caixa {
 
 		private int width ; 
 		private int height;
-		
+		private String caracter;
 //		public Caixa() {
 //		}
 		public Caixa(int width, int height) {
+			this(width, height, "*");
+		}
+		public Caixa(int width, int height, String caracter) {
 			this.width = width;
 			this.height = height;
+			this.caracter = caracter;
 		}
 		public void printBox() {
 			printTOpOrBottom();
@@ -18,18 +22,18 @@ public class Caixa {
 		}
 		public void printTOpOrBottom() {
 			for(int i = 0 ; i < width ; i++) {
-			System.out.print("*");
+			System.out.print(caracter);
 			}
 			System.out.println();
 		}
 		
 		public void printLeftandRigth() {
 			for (int i = 0; i < height - 2; i++) {
-				System.out.print("*");
+				System.out.print(caracter);
 				for (int j = 0; j < width - 2; j++) {
 					System.out.print(" ");
 				}
-				System.out.println("*");
+				System.out.println(caracter);
 			}
 		}
 		
